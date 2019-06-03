70459ad8272301dedecac5f7a681a5460175930e
Stocco Matis <32925370+Rush-er@users.noreply.github.com>
2019-06-03 12:11:14 +0200

    Fixed audio stop

--- spotifyadhost.bat
+++ spotifyadhost.bat
@@ -6,7 +6,6 @@ echo 0.0.0.0	adclick.g.doublecklick.net >> %hostspath%
 echo 0.0.0.0	adeventtracker.spotify.com >> %hostspath%
 echo 0.0.0.0	ads-fa.spotify.com >> %hostspath%
 echo 0.0.0.0	analytics.spotify.com >> %hostspath%
-echo 0.0.0.0	audio2.spotify.com >> %hostspath%
 echo 0.0.0.0	b.scorecardresearch.com >> %hostspath%
 echo 0.0.0.0	bounceexchange.com >> %hostspath%
 echo 0.0.0.0	bs.serving-sys.com >> %hostspath%
@@ -15,14 +14,12 @@ echo 0.0.0.0	core.insightexpressai.com >> %hostspath%
 echo 0.0.0.0	crashdump.spotify.com >> %hostspath%
 echo 0.0.0.0	d2gi7ultltnc2u.cloudfront.net >> %hostspath%
 echo 0.0.0.0	d3rt1990lpmkn.cloudfront.net >> %hostspath%
-echo 0.0.0.0	desktop.spotify.com >> %hostspath%
 echo 0.0.0.0	doubleclick.net >> %hostspath%
 echo 0.0.0.0	ds.serving-sys.com >> %hostspath%
 echo 0.0.0.0	googleadservices.com >> %hostspath%
 echo 0.0.0.0	googleads.g.doubleclick.net >> %hostspath%
 echo 0.0.0.0	gtssl2-ocsp.geotrust.com >> %hostspath%
 echo 0.0.0.0	js.moatads.com >> %hostspath%
-echo 0.0.0.0	log.spotify.com >> %hostspath%
 echo 0.0.0.0	media-match.com >> %hostspath%
 echo 0.0.0.0	omaze.com >> %hostspath%
 echo 0.0.0.0	pagead46.l.doubleclick.net >> %hostspath%
@@ -32,11 +29,9 @@ echo 0.0.0.0	pubads.g.doubleclick.net >> %hostspath%
 echo 0.0.0.0	redirector.gvt1.com >> %hostspath%
 echo 0.0.0.0	s0.2mdn.net >> %hostspath%
 echo 0.0.0.0	securepubads.g.doubleclick.net >> %hostspath%
-echo 0.0.0.0	spclient.wg.spotify.com >> %hostspath%
 echo 0.0.0.0	tpc.googlesyndication.com >> %hostspath%
 echo 0.0.0.0	v.jwpcdn.com >> %hostspath%
 echo 0.0.0.0	video-ad-stats.googlesyndication.com >> %hostspath%
-echo 0.0.0.0	weblb-wg.gslb.spotify.com >> %hostspath%
 echo 0.0.0.0	www.googleadservices.com >> %hostspath%
 echo 0.0.0.0	www.googletagservices.com >> %hostspath%
 echo 0.0.0.0	www.omaze.com >> %hostspath%
