be54d1cc3833e9cea2be8a6e03269040d35141eb
Stocco Matis <32925370+Rush-er@users.noreply.github.com>
2019-06-03 12:12:02 +0200

    Fixed audio stop

--- spotifyadhost.sh
+++ spotifyadhost.sh
@@ -6,7 +6,6 @@ echo -e "
 0.0.0.0	adeventtracker.spotify.com
 0.0.0.0	ads-fa.spotify.com
 0.0.0.0	analytics.spotify.com
-0.0.0.0	audio2.spotify.com
 0.0.0.0	b.scorecardresearch.com
 0.0.0.0	bounceexchange.com
 0.0.0.0	bs.serving-sys.com
@@ -15,7 +14,6 @@ echo -e "
 0.0.0.0	crashdump.spotify.com
 0.0.0.0	d2gi7ultltnc2u.cloudfront.net
 0.0.0.0	d3rt1990lpmkn.cloudfront.net
-0.0.0.0	desktop.spotify.com
 0.0.0.0	doubleclick.net
 0.0.0.0	ds.serving-sys.com
 0.0.0.0	googleadservices.com
@@ -32,11 +30,9 @@ echo -e "
 0.0.0.0	redirector.gvt1.com
 0.0.0.0	s0.2mdn.net
 0.0.0.0	securepubads.g.doubleclick.net
-0.0.0.0	spclient.wg.spotify.com
 0.0.0.0	tpc.googlesyndication.com
 0.0.0.0	v.jwpcdn.com
 0.0.0.0	video-ad-stats.googlesyndication.com
-0.0.0.0	weblb-wg.gslb.spotify.com
 0.0.0.0	www.googleadservices.com
 0.0.0.0	www.googletagservices.com
 0.0.0.0	www.omaze.com
